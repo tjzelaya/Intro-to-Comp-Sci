7d76f18780c27377a7a3eb71f32b87cfa045d6c9
azelaya <azelaya@HS-D210-14.MOHS.local>
2019-05-15 13:21:38 -0400

    incomplete

--- Lesson 7 - scanner/src/NameCount.java
+++ Lesson 7 - scanner/src/NameCount.java
@@ -14,6 +14,7 @@ public class NameCount {
 		wname3 = (wname.substring(0,wname2));
 		System.out.println(wname3);
 		
+		
 
 	}
 
---
+++ Lesson 7 - scanner/src/pythagorastheorus.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class pythagorastheorus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please enter your A value");
+		int avalue = userInput.nextInt();
+		System.out.println("Please enter your B value");
+		int bvalue = userInput.nextInt();
+		double 
+		System.out.println("Square root of " + avalue + " squared " + bvalue + "squared = " + cvalue);
+
+	}
+
+}
