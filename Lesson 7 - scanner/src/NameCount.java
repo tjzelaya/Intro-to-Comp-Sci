510176b650dbff7c74b07584b54d4c1969c12723
azelaya <azelaya@HS-D210-14.MOHS.local>
2019-05-15 13:11:38 -0400

    Added length count

---
+++ Lesson 7 - scanner/src/NameCount.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NameCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner userInput = new Scanner(System.in);
+		String wname; int wname2; String wname3;
+		System.out.println("Please enter your whole name.");
+		wname = userInput.nextLine();
+		System.out.println("Your name has " + wname.length() + " characters in it, how many would you like me to print?");
+		wname2 = userInput.nextInt();
+		wname3 = (wname.substring(0,wname2));
+		System.out.println(wname3);
+		
+
+	}
+
+}
--- Lesson 7 - scanner/src/averageprogram.java
+++ Lesson 7 - scanner/src/averageprogram.java
@@ -5,8 +5,8 @@ public class averageprogram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner inputScan;
-		inputScan = new Scanner(System.in);
+	
+		Scanner inputScan = new Scanner(System.in);
 		int t1; int t3; int t2;
 		System.out.println("Test 1 grade?");
 		t1 = inputScan.nextInt();
@@ -19,6 +19,16 @@ public class averageprogram {
 		System.out.println("Test3 grade : " + t3);
 		double average = ((t1 + t2 + t3)/3.0);
 		System.out.println("average : " + average);
+		
+		
+		Scanner userInput = new Scanner(System.in); 
+		String fname; String lname;
+		System.out.println("What is your first name?");
+		fname = userInput.nextLine();
+		System.out.println("What is your last name?");
+		lname = userInput.nextLine();
+		System.out.println("Your full name is " + fname + " " + lname +".");
+		
 		
 		
 
